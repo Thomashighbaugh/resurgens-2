fc0a8fe9e840378a0cf86be96a1384dcce56fce6
tlh <thighbaugh@zoho.com>
2018-11-11 17:29:58 -0800

    v 2.0.7 Filling out

--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
-let bgColor;
-let effect = 'animated bounceInLeft'; /* bounceIn, bounceInUp, bounceInDown, bounceInLeft,
-										 bounceInRight, rotateIn, rotateInUpLeft, rotateInDownLeft,
-										 rotateInUpRight, rotateInDownRight  */
+//This file is the ES06 used by the site. While ES05 would have worked just as well, using Let instead of Var is a minor
+//point and demonstrating knowledge of JS was the point so here is the product.
+
+let bgColor; //used for holding color of the card to be used as background!
+
+let effect = 'animated bounceInLeft';
+/* bounceIn, bounceInUp, bounceInDown, bounceInLeft,
+                                        bounceInRight, rotateIn, rotateInUpLeft, rotateInDownLeft,
+                                        rotateInUpRight, rotateInDownRight  */
 //hide the content of the back side of the cards
 $('.all-content').hide();
 //function to both change the display of the card to show the content contained
-$('.content li').click(function(){
-	$('.card-front, .card-back').hide();
-	$('.content li').removeClass('active').hide().css('border','none');
-	$(this).addClass('active').show(); //shows card content
-	bgColor = $('.active .card-back').css('background-color');
-	$('.content').css('background-color',bgColor);
-	$('.close, .all-content').show();
-	$('.responsive').append('<span class="close">close</span>').addClass(effect);
-});
+$('.content li').click(function () {
+    $('.card-front, .card-back').hide();
+    $('.content li').removeClass('active').hide().css('border', 'none');
+    $(this).addClass('active').show(); //shows card content
+    bgColor = $('.active .card-back').css('background-color');
+    $('.content').css('background-color', bgColor);
+    $('.close, .all-content').show();
+    $('.responsive').append('<span class="close">close</span>').addClass(effect);
+});//end the opening content function
 
-
+//creates the close button functionality
 $('.responsive').on('click', '.close', function(){
-
+//close function definition that "undoes" what was done prior
 	$('.close').remove();
-	bgColor = $('.active .card-front').css('background-color');
+    bgColor = $('.active .card-front').css('background-color'); //restores background
 	$('.responsive').removeClass(effect);
 	$('.all-content').hide();
 	$('.content li').removeClass('active').show().css({ 'border-bottom':'1px solid #2c2c2c',
-													    'border-left':'1px solid #2c2c2c' });
+        'border-left': '1px solid #2c2c2c'
+    }); //recreates borders for cards
 	$('.card-front, .card-back').show();
 	$('.content').css('background-color',bgColor);
-});
+}); //ends restore main function
