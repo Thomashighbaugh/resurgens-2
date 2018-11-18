0da727c757c3849570db2a1f4cef6c3df6dfe539
tlh <thighbaugh@zoho.com>
2018-11-17 23:01:49 -0800

    added a handy new error message that would have saved my debugging process and will be added to dependency laden projects thus forth

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
+//boilerplate error that most likely only I will see that would have saved me 2 hours of debugging in producing this site as only with
+//jQuery included on the server would the UI render correctly
 if (typeof jQuery === 'undefined') {
-    throw new Error('Bootstrap\'s JavaScript requires jQuery')
+    throw new Error('This site requires requires jQuery, if you need to download it (because you use an older browser) use NPM, ' +
+        'Yarn, Google Search jQuery, update your browser or if you are TLH, include the minified version when uploading ')
 }
 
 var bgColor;
